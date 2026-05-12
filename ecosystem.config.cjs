b052d521be15ae584e324b1d3ee958697bb03ec4
gowtdy <gowtd@gmail.com>
2026-05-12 15:21:36 +0800

    port 4300

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'MoGoFun-4100',
+      name: 'MoGoFun-4300',
       script: '/home/aigc/service/voice/web_dist/mogofun_dist/server/index.mjs',
       instances: 2,
       exec_mode: 'cluster',

faacf7ac24f836f18bdc87669f983b95f2753dca
gowtdy <gowtd@gmail.com>
2026-05-12 12:17:54 +0800

    add

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'MoGoFun-4100',
+      script: '/home/aigc/service/voice/web_dist/mogofun_dist/server/index.mjs',
+      instances: 2,
+      exec_mode: 'cluster',
+      env: {
+        PORT: 4100,
+        HOST: '0.0.0.0',
+        NODE_PATH: '/home/aigc/service/voice/web_dist/mogofun_dist/server/node_modules'
+      },
+      time: true
+    }
+  ]
+}

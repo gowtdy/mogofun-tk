31944188307640b5885a40fff69711be2afed0e9
gowtdy <gowtd@gmail.com>
2026-05-17 00:08:24 +0800

    vocal isolator

--- pages/vocal-isolator.vue
+++ pages/vocal-isolator.vue
@@ -7,43 +7,9 @@
 </template>
 
 <script setup lang="ts">
-import SeparationPage, { type SeparationUploadConfig } from '~/components/SeparationPage.vue'
-import { useActionReporter, ActionType } from '~/composables/actionReporter'
+import SeparationPage from '~/components/SeparationPage.vue'
 import {
-  SparklesIcon,
-  MusicalNoteIcon,
-  CpuChipIcon,
-  AdjustmentsHorizontalIcon,
-  ArrowDownTrayIcon,
-  WrenchScrewdriverIcon,
-  UserGroupIcon,
-  BeakerIcon
-} from '@heroicons/vue/24/outline'
-
-const vocalIsolatorUpload: SeparationUploadConfig = {
-  mediaType: 'vocal isolator',
-  modelCategory: 'vocal isolator',
-  modelName: '',
-  apiEndpoint: '/coverapi/vocal',
-  actionType: {
-    extract: ActionType.VOCAL_EXTRACT,
-    login: ActionType.VOCAL_EXTRACT_LOGIN,
-    subscript: ActionType.VOCAL_EXTRACT_SUBSCRIPT,
-    download: ActionType.VOCAL_DOWNLOAD,
-    downloadLogin: ActionType.VOCAL_DOWNLOAD_LOGIN,
-    downloadSubscript: ActionType.VOCAL_DOWNLOAD_SUBSCRIPT,
-    upload: ActionType.VOCAL_EXTRACT_UPLOAD
-  }
-}
-
-const vocalIsolatorAdvantageIcons = [
-  SparklesIcon,
-  MusicalNoteIcon,
-  CpuChipIcon,
-  AdjustmentsHorizontalIcon,
-  ArrowDownTrayIcon,
-  WrenchScrewdriverIcon,
-  UserGroupIcon,
-  BeakerIcon
-]
+  vocalIsolatorUpload,
+  vocalIsolatorAdvantageIcons
+} from '~/config/vocalIsolatorSeparation'
 </script>

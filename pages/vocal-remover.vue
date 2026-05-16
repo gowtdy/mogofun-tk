1743eae1c129c3f2f97bf516e66e9415dea6e7d1
gowtdy <gowtd@gmail.com>
2026-05-17 00:08:40 +0800

    vocal remover

--- pages/vocal-remover.vue
+++ pages/vocal-remover.vue
@@ -8,43 +8,13 @@
 </template>
 
 <script setup lang="ts">
-import SeparationPage, { type SeparationUploadConfig } from '~/components/SeparationPage.vue'
-import { useActionReporter, ActionType } from '~/composables/actionReporter'
+import SeparationPage from '~/components/SeparationPage.vue'
 import {
-  SparklesIcon,
-  MusicalNoteIcon,
-  CpuChipIcon,
-  ArrowDownTrayIcon,
-  AdjustmentsHorizontalIcon,
-  WrenchScrewdriverIcon
-} from '@heroicons/vue/24/outline'
+  vocalRemoverUpload,
+  vocalRemoverAdvantageIcons
+} from '~/config/vocalRemoverSeparation'
 
 definePageMeta({
   layout: 'footerlink'
 })
-
-const vocalRemoverUpload: SeparationUploadConfig = {
-  mediaType: 'audio',
-  modelCategory: 'vocal remover',
-  modelName: 'vocal remover',
-  apiEndpoint: '/coverapi/instrument',
-  actionType: {
-    extract: ActionType.VOCAL_REMOVE,
-    login: ActionType.VOCAL_REMOVE_LOGIN,
-    subscript: ActionType.VOCAL_REMOVE_SUBSCRIPT,
-    download: ActionType.VOCAL_REMOVE_DOWNLOAD,
-    downloadLogin: ActionType.VOCAL_REMOVE_DOWNLOAD_LOGIN,
-    downloadSubscript: ActionType.VOCAL_REMOVE_DOWNLOAD_SUBSCRIPT,
-    upload: ActionType.VOCAL_REMOVE_UPLOAD
-  }
-}
-
-const vocalRemoverAdvantageIcons = [
-  SparklesIcon,
-  MusicalNoteIcon,
-  CpuChipIcon,
-  ArrowDownTrayIcon,
-  AdjustmentsHorizontalIcon,
-  WrenchScrewdriverIcon
-]
 </script>

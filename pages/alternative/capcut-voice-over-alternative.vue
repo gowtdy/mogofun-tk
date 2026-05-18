72297ad5476c0778aa46bdc8fedf6bc667dbd6ba
gowtdy <gowtd@gmail.com>
2026-05-18 20:09:56 +0800

    add

---
+++ pages/alternative/capcut-voice-over-alternative.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexGenericPage page-slug="capcut-voice-over-alternative" />
+</template>
+
+<script setup lang="ts">
+import IndexGenericPage from '~/components/IndexGenericPage.vue'
+</script>

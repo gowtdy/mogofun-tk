80c3bcd4fe18fc26f6a9232a95af365910b25bbd
gowtdy <gowtd@gmail.com>
2026-05-18 20:09:42 +0800

    add

---
+++ pages/aivoice/text-to-speech-generator.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexGenericPage page-slug="text-to-speech-generator" />
+</template>
+
+<script setup lang="ts">
+import IndexGenericPage from '~/components/IndexGenericPage.vue'
+</script>

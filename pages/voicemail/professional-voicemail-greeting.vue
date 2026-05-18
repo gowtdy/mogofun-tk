048701df2b130dfd2ddf4f1113bbdcaefdc40797
gowtdy <gowtd@gmail.com>
2026-05-18 23:35:26 +0800

    add

---
+++ pages/voicemail/professional-voicemail-greeting.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexGenericPage page-slug="professional-voicemail-greeting" />
+</template>
+
+<script setup lang="ts">
+import IndexGenericPage from '~/components/IndexGenericPage.vue'
+</script>
---
+++ pages/voicemail/short-funny-voicemail-greetings.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexGenericPage page-slug="short-funny-voicemail-greetings" />
+</template>
+
+<script setup lang="ts">
+import IndexGenericPage from '~/components/IndexGenericPage.vue'
+</script>

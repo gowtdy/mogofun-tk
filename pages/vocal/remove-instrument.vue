b2e94b6d697ec7d63c13018fa37fd418959cb33f
gowtdy <gowtd@gmail.com>
2026-05-18 10:59:42 +0800

    add

---
+++ pages/vocal/remove-instrument.vue
@@ -0,0 +1,16 @@
+<template>
+  <SeparationPage
+    page-slug="remove-instrument"
+    common-page-slug="vocal-isolator"
+    :upload="vocalIsolatorUpload"
+    :advantage-icons="vocalIsolatorAdvantageIcons"
+  />
+</template>
+
+<script setup lang="ts">
+import SeparationPage from '~/components/SeparationPage.vue'
+import {
+  vocalIsolatorUpload,
+  vocalIsolatorAdvantageIcons
+} from '~/config/vocalIsolatorSeparation'
+</script>

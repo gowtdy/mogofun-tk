d4cb27111930adffdf015a8ef379da43c9c05919
gowtdy <gowtd@gmail.com>
2026-05-18 20:10:37 +0800

    add

---
+++ pages/tiktok/tiktok-text-to-speech.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexGenericPage page-slug="tiktok-text-to-speech" />
+</template>
+
+<script setup lang="ts">
+import IndexGenericPage from '~/components/IndexGenericPage.vue'
+</script>

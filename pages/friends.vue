d08dbf795408ff4d0187b0457e35bf152e8ae2a8
gowtdy <gowtd@gmail.com>
2026-06-01 21:24:42 +0800

    friends

--- pages/friends.vue
+++ pages/friends.vue
@@ -15,5 +15,12 @@
 
 <script setup>
 const friendLinks = ref([
+  { title: 'MoGoFun Strip Climate', url: 'https://climate.stripe.com/IUM2MG' },
+  { title: 'AIVoiceLab frinds', url: 'https://aivoicelab.net/en/friends' },
+  { title: 'medium', url: 'https://medium.com/@tiktokaivoicetool/5%E4%B8%AA%E6%9C%80%E4%BD%B3%E5%85%8D%E8%B4%B9ai%E9%9F%B3%E6%95%88%E7%94%9F%E6%88%90%E5%99%A8%E6%8E%A8%E8%8D%90-ad53838a64c7' },
+  { title: 'url tw', url: 'https://myshare.url.com.tw/note/2428720' },
+  { title: 'thosefree', url: 'https://www.thosefree.com/tiktokvoice' },
+  { title: 'pixnet', url: 'https://keychtouch.pixnet.net/blog/posts/900657311119583110' },
+  { title: 'chtouch', url: 'https://chtouch.com/mogofun-tiktok-voice-generator/' },
 ])
 </script>

5d6712fdfe3ca7b5599bd506359f52c67f5a352e
gowtdy <gowtd@gmail.com>
2026-06-03 12:54:25 +0800

    add link

--- pages/friends.vue
+++ pages/friends.vue
@@ -27,5 +27,12 @@ const friendLinks = ref([
   { title: 'dpdns', url: 'https://xiaoluo3.dpdns.org/wap/deal.php?code=lxBbi&dataID=22054&mudi=download&point=0' },
   { title: 'notion', url: 'https://goofy-scarf-8ca.notion.site/API-handbook-of-MoGoFun-15154f4a5c9a80d78e79d04d2bbd4f60' },
   { title: 'w2solo', url: 'https://w2solo.com/topics/4908' },
+  { title: 'youtube' url: 'https://www.youtube.com/redirect?q=https://mogofun.com' },
+  { title: 'zhihu', url: 'https://link.zhihu.com/?target=https%3A%2F%2Fmogofun.com' },
+  { title: 'scamadviser', url: 'https://www.scamadviser.com/check-website/mogofun.com' },
+  { title: 'reddit', url: 'https://www.reddit.com/r/chtouch_com/comments/1tpxbcs/mogofun_tiktok_%E8%AA%9E%E9%9F%B3%E7%94%9F%E6%88%90%E5%99%A8%E7%94%9F%E6%88%90%E5%90%84%E7%A8%AE_tiktok_%E8%B6%A3%E5%91%B3_ai_%E8%AA%9E%E9%9F%B3%E5%A4%9A%E7%A8%AE%E8%A7%92%E8%89%B2%E5%AE%8C%E5%85%A8%E5%85%8D%E8%B2%BB/' },
+  { title: 'magicly', url: 'https://magic.ly/mogofun' },
+  { title: 'linktr', url: 'https://linktr.ee/bejamin1995' },
+  { title: 'twitter', url: 'https://x.com/aurthur711335' },
 ])
 </script>

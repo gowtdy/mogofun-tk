f90930dff74397cb05d4287bff74bca39929f1be
gowtdy <gowtd@gmail.com>
2026-08-14 19:28:32 +0800

    geo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,16 @@
     <main id="main-content">
       <slot />
     </main>
+    <div class="bg-white pb-4 md:pb-6">
+      <CitationQuoteSection />
+    </div>
     <Footer />
   </div>
 </template>
 
 <script setup>
 import Footer from '~/components/Footer.vue'
+import CitationQuoteSection from '~/components/CitationQuoteSection.vue'
 </script>
 
 <style scoped>
